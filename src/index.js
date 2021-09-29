a8fc74de95f8e712c78685de69851b1622c02bc8
nitinmittal23 <nitinmittal44066@gmail.com>
2021-09-29 21:39:39 +0530

    fix: error handling for spoofing

--- src/index.js
+++ src/index.js
@@ -61,6 +61,21 @@ console.log(swaggerDocs.paths)
 // api router
 app.use('/api/v1', routes)
 
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
 // Start Server
 app.listen(process.env.PORT, function() {
   console.log('Nonce Tracker Has been started')
